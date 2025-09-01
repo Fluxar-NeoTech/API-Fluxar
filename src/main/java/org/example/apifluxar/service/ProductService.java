aab2b5c94505611f8af6fad9c274b6d4e061ff1e
DudaCacciatore <mdoriacacciatore@gmail.com>
2025-09-01 11:08:51 -0300

    feat: add classes relacionadas a tabela produtoi

---
+++ src/main/java/org/example/apifluxar/service/ProductService.java
@@ -0,0 +1,41 @@
+package org.example.apifluxar.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.apifluxar.dto.ProductRequestDTO;
+import org.example.apifluxar.dto.ProductResponseDTO;
+import org.example.apifluxar.model.Product;
+import org.example.apifluxar.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    ProductRepository productRepository;
+    ObjectMapper objectMapper;
+
+    public ProductService(ProductRepository productRepository, ObjectMapper objectMapper) {
+        this.productRepository = productRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    public ProductResponseDTO getProductByName(String name) {
+        Product product = productRepository.findByName(name).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return objectMapper.convertValue(product, ProductResponseDTO.class);
+    }
+
+    public List<ProductResponseDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return objectMapper.convertValue(products, new TypeReference<List<ProductResponseDTO>>() {});
+    }
+
+    public ProductResponseDTO createProduct(ProductRequestDTO productRequestDTO) {
+        Product product = objectMapper.convertValue(productRequestDTO, Product.class);
+        Product savedProduct = productRepository.save(product);
+        return objectMapper.convertValue(savedProduct, ProductResponseDTO.class);
+    }
+}
