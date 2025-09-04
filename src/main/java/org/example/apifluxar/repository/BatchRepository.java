46db6fab38224209c2fbe0b0334b0cba3540d802
heloisa-machado <heloisa.machado@germinare.org.br>
2025-09-04 08:37:46 -0300

    fix: retirando arquivos a mais

--- src/main/java/org/example/apifluxar/repository/BatchRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.apifluxar.repository;
-
-import org.example.apifluxar.model.Batch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BatchRepository extends JpaRepository<Batch, Long> {
-}
