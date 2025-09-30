b7a802afe772f1b2a8a7156ac7d794505a871665
heloisa-machado <heloisa.machado@germinare.org.br>
2025-09-30 08:24:38 -0300

    fix: arrumando o metoto de login

--- src/main/java/org/example/apifluxar/dto/employee/EmployeeRequestDTO.java
+++ src/main/java/org/example/apifluxar/dto/employee/EmployeeRequestDTO.java
@@ -3,16 +3,17 @@ package org.example.apifluxar.dto.employee;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.example.apifluxar.validation.OnCreate;
 
 public class EmployeeRequestDTO {
     @NotNull(message = "O email não pode ser nulo", groups = OnCreate.class)
-    @Min(value = 5, message = "O email deve ter no mínimo 5 caracteres", groups = OnCreate.class)
+    @Size(min = 5, message = "O email deve ter no mínimo 5 caracteres", groups = OnCreate.class)
     @NotBlank(message = "O email não pode ser em branco", groups = OnCreate.class)
     private String email;
 
     @NotNull(message = "A senha não pode ser nula", groups = OnCreate.class)
-    @Min(value = 6, message = "A senha deve ter no mínimo 6 caracteres", groups = OnCreate.class)
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres", groups = OnCreate.class)
     @NotBlank(message = "A senha não pode ser em branco", groups = OnCreate.class)
     private String password;
 
